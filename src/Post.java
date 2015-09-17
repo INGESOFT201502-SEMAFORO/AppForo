faf36c31763eb7e5fb59e9913f7482e77365df07
Juan Pablo Arias <jpariasc@unal.edu.co>
2015-09-17 12:38:38 -0500

    Clase Post con Métodos getters y setters

---
+++ src/Post.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by Juan on 17/09/2015.
+ */
+public class Post {
+    private String topic;
+    private Date dateCreated;
+    private Date lastUpdate;
+    private boolean isAllowed;
+    private ArrayList<File> attachments;
+
+    public Post(String topic, Date dateCreated, Date lastUpdate, boolean isAllowed) {
+        this.topic = topic;
+        this.dateCreated = dateCreated;
+        this.lastUpdate = lastUpdate;
+        this.isAllowed = isAllowed;
+        this.attachments = new ArrayList<File>();
+    }
+
+    public void attachFile(File attachment) {
+        this.attachments.add(attachment);
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public boolean isAllowed() {
+        return isAllowed;
+    }
+
+    public void setIsAllowed(boolean isAllowed) {
+        this.isAllowed = isAllowed;
+    }
+}
